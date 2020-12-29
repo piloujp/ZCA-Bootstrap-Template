60c20c7058e0b9de3c8a7d63cf3ce87a66aada70
lat9 <cindy@vinosdefrutastropicales.com>
2020-12-29 10:38:11 -0500

    #17, #30: Update readme to identify changes; bump version to v3.1.0.

--- includes/templates/bootstrap/common/tpl_columnar_display.php
+++ includes/templates/bootstrap/common/tpl_columnar_display.php
@@ -2,7 +2,7 @@
 /**
  * Common Template - tpl_columnar_display.php
  *
- * BOOTSTRAP v3.0.1
+ * BOOTSTRAP v3.1.0
  *
  * This file is used for generating columnar output where needed, based on the supplied array of table-cell contents.
  *
