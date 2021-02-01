91d3ade3da858855115e1de848f3a048b3b366a8
lat9 <cindy@vinosdefrutastropicales.com>
2021-02-01 08:57:24 -0500

    #46: Update order-totals' display to accept a column-count.

--- includes/templates/bootstrap/templates/tpl_modules_order_totals.php
+++ includes/templates/bootstrap/templates/tpl_modules_order_totals.php
@@ -2,7 +2,7 @@
 /**
  * Module Template
  * 
- * BOOTSTRAP v3.0.0
+ * BOOTSTRAP v3.1.0
  *
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -10,6 +10,14 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_modules_order_totals.php 2993 2006-02-08 07:14:52Z birdbrain $
  */
+// -----
+// If a column-span for the title is provided, use it; otherwise, default to 3.
+//
+// Note: Globalization is required since this function is called from the order_total class'
+// output method.
+//
+global $zca_bootstrap_ot_colspan;
+$zca_bootstrap_ot_colspan = (!empty($zca_bootstrap_ot_colspan)) ? $zca_bootstrap_ot_colspan : '3';
 
 /**
  * Displays order-totals modules' output, as called from order_total::output
@@ -17,7 +25,7 @@
 for ($i = 0; $i < $size; $i++) { 
 ?>
 <tr id="<?php echo str_replace('_', '', $GLOBALS[$class]->code); ?>">
-    <td colspan="3" class="ot-title"><?php echo $GLOBALS[$class]->output[$i]['title']; ?></td>
+    <td colspan="<?php echo $zca_bootstrap_ot_colspan; ?>" class="ot-title"><?php echo $GLOBALS[$class]->output[$i]['title']; ?></td>
     <td class="ot-text"><?php echo $GLOBALS[$class]->output[$i]['text']; ?></td> 
 </tr>
 <?php 
