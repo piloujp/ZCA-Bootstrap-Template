35739ba055e132c30bf357c47e0d1b9bff2e8914
lat9 <cindy@vinosdefrutastropicales.com>
2021-07-04 09:27:53 -0400

    #61: Correcting ghosted border on possibly-hidden columns.

--- includes/templates/bootstrap/templates/tpl_shopping_cart_default.php
+++ includes/templates/bootstrap/templates/tpl_shopping_cart_default.php
@@ -66,11 +66,11 @@ if ($flagHasCartContents) {
         <table id="shoppingCartDefault-cartTableDisplay" class="cartTableDisplay table table-bordered table-striped">
             <tr>
                 <th scope="col" id="cartTableDisplay-qtyHeading"><?php echo TABLE_HEADING_QUANTITY; ?></th>
-                <th scope="col" class="d-none d-sm-block" id="cartTableDisplay-qtyUpdateHeading">&nbsp;</th>
+                <th scope="col" class="d-none d-sm-table-cell" id="cartTableDisplay-qtyUpdateHeading">&nbsp;</th>
                 <th scope="col" id="cartTableDisplay-productsHeading"><?php echo TABLE_HEADING_PRODUCTS; ?></th>
                 <th scope="col" id="cartTableDisplay-priceHeading"><?php echo TABLE_HEADING_PRICE; ?></th>
                 <th scope="col" id="cartTableDisplay-totalsHeading"><?php echo TABLE_HEADING_TOTAL; ?></th>
-                <th scope="col" class="d-none d-sm-block" id="cartTableDisplay-removeHeading">&nbsp;</th>
+                <th scope="col" class="d-none d-sm-table-cell" id="cartTableDisplay-removeHeading">&nbsp;</th>
             </tr>
 <?php
     foreach ($productArray as $product) {
@@ -85,7 +85,7 @@ if ($flagHasCartContents) {
         }
 ?>
                 </td>
-                <td class="qtyUpdateCell text-center d-none d-sm-block"><?php echo (!empty($product['buttonUpdate'])) ? $product['buttonUpdate'] : ''; ?></td>
+                <td class="qtyUpdateCell text-center d-none d-sm-table-cell"><?php echo (!empty($product['buttonUpdate'])) ? $product['buttonUpdate'] : ''; ?></td>
                 <td class="productsCell">
                     <a href="<?php echo $product['linkProductsName']; ?>"><span class="d-none d-sm-block"><?php echo $product['productsImage']; ?></span><?php echo $product['productsName'] . '' . $product['flagStockCheck'] . ''; ?></a>
 
@@ -110,7 +110,7 @@ if ($flagHasCartContents) {
                 </td>
                 <td class="priceCell"><?php echo $product['productsPriceEach']; ?></td>
                 <td class="totalsCell"><?php echo $product['productsPrice']; ?></td>
-                <td class="removeCell text-center d-none d-sm-block">
+                <td class="removeCell text-center d-none d-sm-table-cell">
 <?php
         if ($product['buttonDelete']) {
 ?>
