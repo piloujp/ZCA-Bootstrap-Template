bc6e68eb96e0fd5e601eeeef1c6c7ae307bf9cd8
lat9 <cindy@vinosdefrutastropicales.com>
2022-04-08 12:10:42 -0400

    #118: Correcting HTML validation issue on pagination links' ellipsis element.

--- includes/classes/zca/zca_split_page_results.php
+++ includes/classes/zca/zca_split_page_results.php
@@ -2,7 +2,7 @@
 /**
  * split_page_results Class.
  *
- * BOOTSTRAP v3.1.4
+ * BOOTSTRAP v3.3.0
  *
  * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
@@ -187,7 +187,7 @@ class zca_splitPageResults extends base
 
         // next group of pages
         if ($cur_window_num < $max_window_num) {
-            $link = '<a class="page-link" href="' . zen_href_link($_GET['main_page'], $parameters . $this->page_name . '=' . (($cur_window_num) * $max_page_links + 1), $request_type) . '" title="' . sprintf(PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE, $max_page_links) . '" aria-label="' . ARIA_PAGINATION_ELLIPSIS_NEXT . '">...</a>';
+            $link = '<li><a class="page-link" href="' . zen_href_link($_GET['main_page'], $parameters . $this->page_name . '=' . (($cur_window_num) * $max_page_links + 1), $request_type) . '" title="' . sprintf(PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE, $max_page_links) . '" aria-label="' . ARIA_PAGINATION_ELLIPSIS_NEXT . '">...</a></li>';
             $display_links_string .= $link . '&nbsp;';
             $ul_elements .= '  <li class="ellipsis page-item">' . $link . '</li>' . "\n";
         } else {
