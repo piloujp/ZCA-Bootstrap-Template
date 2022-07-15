a756b5286b309f224823d8a23dafbb8bbebe4304
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-15 08:53:19 -0400

    #131: Refactoring category_row.php module to mimic zc158 implementation.

--- includes/modules/bootstrap/category_row.php
+++ includes/modules/bootstrap/category_row.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * BOOTSTRAP v3.1.6
+ * BOOTSTRAP v3.4.0
  *
  * index category_row.php
  *
- * Prepares the content for displaying a category's sub-category listing in grid format.  
+ * Prepares the content for displaying a category's sub-category listing in grid format.
  * Once the data is prepared, it calls the standard tpl_list_box_content template for display.
  *
- * @package page
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: category_row.php 4084 2006-08-06 23:59:36Z drbyte $
+ * @version $Id: Scott C Wilson 2021 Jul 28 Modified in v1.5.8-alpha $
  */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
@@ -23,13 +22,14 @@ $row = 0;
 $col = 0;
 $list_box_contents = [];
 if ($num_categories > 0) {
-    if ($num_categories < MAX_DISPLAY_CATEGORIES_PER_ROW || MAX_DISPLAY_CATEGORIES_PER_ROW == 0) {
+    if ($num_categories < MAX_DISPLAY_CATEGORIES_PER_ROW || MAX_DISPLAY_CATEGORIES_PER_ROW === '0') {
         $col_width = floor(100/$num_categories);
     } else {
         $col_width = floor(100/MAX_DISPLAY_CATEGORIES_PER_ROW);
     }
 
     foreach ($categories as $next_category) {
+        $zco_notifier->notify('NOTIFY_CATEGORY_ROW_IMAGE', $next_category['categories_id'], $next_category['categories_image']); 
         if (empty($next_category['categories_image'])) {
             $next_category['categories_image'] = 'pixel_trans.gif';
         }
@@ -46,7 +46,7 @@ if ($num_categories > 0) {
         ];
 
         $col++;
-        if ($col > (MAX_DISPLAY_CATEGORIES_PER_ROW -1)) {
+        if ($col >= MAX_DISPLAY_CATEGORIES_PER_ROW) {
             $col = 0;
             $row++;
         }
