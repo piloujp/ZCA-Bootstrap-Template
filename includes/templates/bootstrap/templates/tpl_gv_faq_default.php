a6326605ff84b5c46a658bf930b4cfc23b0cd5a0
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-15 14:17:14 -0400

    #314: Updating the `gv_faq` page's template for zc157/zc158 compatibility.

--- includes/templates/bootstrap/templates/tpl_gv_faq_default.php
+++ includes/templates/bootstrap/templates/tpl_gv_faq_default.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * Page Template
- * 
- * BOOTSTRAP v3.0.0
+ * BOOTSTRAP v3.4.0
  *
  * Displays the FAQ pages for the Gift-Certificate/Voucher system.<br />
  *
@@ -12,61 +10,46 @@
  * @version $Id: mc12345678 2019 Nov 14 Modified in v1.5.7 $
  */
 ?>
-
 <div id="gvFaqDefault" class="centerColumn">
-    
 <?php
 // only show when there is a GV balance
-  if (!empty($customer_has_gv_balance) ) {
+if (!empty($customer_has_gv_balance)) {
+    require $template->get_template_dir('tpl_modules_send_or_spend.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_modules_send_or_spend.php';
+}
+
+// -----
+// zc158+ now determines the sub-heading title/text and supplies a variable
+// containing the value; previous versions provided the information via
+// a language define.
+//
+$subHeadingTitle ??= SUB_HEADING_TITLE;
+$subHeadingText ??= SUB_HEADING_TEXT;
 ?>
-
-<?php require($template->get_template_dir('tpl_modules_send_or_spend.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_send_or_spend.php'); ?>
-
-<?php
-  }
-?>
-
-<!--bof gift certificate faq card-->
-<div id="giftCertificateFaq-card" class="card mb-3">
-<h4 id="giftCertificateFaq-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
-<div id="giftCertificateFaq-card-body" class="card-body p-3"> 
-
-<div id="giftCertificateFaq-content" class="content"><?php echo TEXT_INFORMATION; ?></div>
-
-<h2 id="giftCertificateFaq-subHeading" class="pageSubHeading"><?php echo SUB_HEADING_TITLE; ?></h2>
-
-<div id="giftCertificateFaq-content-two" class="content"><?php echo SUB_HEADING_TEXT; ?></div>
-
-<div id="giftCertificateFaq-btn-toolbar" class="btn-toolbar my-3" role="toolbar">
-<?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
-</div>
-
-</div>
+    <div id="giftCertificateFaq-card" class="card mb-3">
+        <h4 id="giftCertificateFaq-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
+        <div id="giftCertificateFaq-card-body" class="card-body p-3"> 
+            <div id="giftCertificateFaq-content" class="content"><?php echo TEXT_INFORMATION; ?></div>
+            <h2 id="giftCertificateFaq-subHeading" class="pageSubHeading"><?php echo $subHeadingTitle; ?></h2>
+            <div id="giftCertificateFaq-content-two" class="content"><?php echo $subHeadingText; ?></div>
+            <div id="giftCertificateFaq-btn-toolbar" class="btn-toolbar my-3" role="toolbar">
+                <?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
+            </div>
+        </div>
     </div>
-<!--eof gift certificate faq card-->
 
-<!--bof please enter your gift certificate redemption code card-->
-<div id="giftCertificateRedemption-card" class="card">
-  <h4 id="giftCertificateRedemption-card-header" class="card-header">
-<?php echo TEXT_GV_REDEEM_INFO; ?>
-  </h4>
-  <div id="giftCertificateRedemption-card-body" class="card-body">
-      
-<form action="<?php echo zen_href_link(FILENAME_GV_REDEEM, '', 'NONSSL', false); ?>" method="get">
-<?php echo zen_draw_hidden_field('main_page',FILENAME_GV_REDEEM) . zen_draw_hidden_field('goback','true') . zen_hide_session_id(); ?>      
+    <div id="giftCertificateRedemption-card" class="card">
+        <h4 id="giftCertificateRedemption-card-header" class="card-header"><?php echo TEXT_GV_REDEEM_INFO; ?></h4>
+        <div id="giftCertificateRedemption-card-body" class="card-body">
+            <form action="<?php echo zen_href_link(FILENAME_GV_REDEEM, '', 'NONSSL', false); ?>" method="get">
+                <?php echo zen_draw_hidden_field('main_page',FILENAME_GV_REDEEM) . zen_draw_hidden_field('goback','true') . zen_hide_session_id(); ?>
 
-<label class="inputLabel" for="lookup-gv-redeem"><?php echo TEXT_GV_REDEEM_ID; ?></label>
-<?php echo zen_draw_input_field('gv_no', isset($_GET['gv_no']) ? $_GET['gv_no'] : '0', 'size="18" id="lookup-gv-redeem"');?>
+                <label class="inputLabel" for="lookup-gv-redeem"><?php echo TEXT_GV_REDEEM_ID; ?></label>
+                <?php echo zen_draw_input_field('gv_no', isset($_GET['gv_no']) ? $_GET['gv_no'] : '0', 'size="18" id="lookup-gv-redeem"');?>
 
-<div id="giftCertificateRedemption-btn-toolbar" class="btn-toolbar justify-content-end my-3" role="toolbar">
-<?php echo zen_image_submit(BUTTON_IMAGE_REDEEM, BUTTON_REDEEM_ALT); ?>
-</div>
-
-</form>
-
-  </div>
-</div>
-<!--eof please enter your gift certificate redemption code card-->
-
-
+                <div id="giftCertificateRedemption-btn-toolbar" class="btn-toolbar justify-content-end my-3" role="toolbar">
+                    <?php echo zen_image_submit(BUTTON_IMAGE_REDEEM, BUTTON_REDEEM_ALT); ?>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
