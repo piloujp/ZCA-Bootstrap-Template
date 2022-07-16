660cd297b335dbb936e2ae7878769ed9d21afad1
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-16 09:22:44 -0400

    #134: password_forgotten, refactoring, adding margin below the upper instructions and changing the email-address autocomplete to 'username' as in zc158.

--- includes/templates/bootstrap/templates/tpl_password_forgotten_default.php
+++ includes/templates/bootstrap/templates/tpl_password_forgotten_default.php
@@ -2,7 +2,7 @@
 /**
  * Page Template
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.4.0
  *
  * @package templateSystem
  * @copyright Copyright 2003-2016 Zen Cart Development Team
@@ -12,29 +12,27 @@
  */
 ?>
 <div id="passwordForgottenDefault" class="centerColumn">
+    <?php echo zen_draw_form('password_forgotten', zen_href_link(FILENAME_PASSWORD_FORGOTTEN, 'action=process', 'SSL')); ?>
+<?php
+if ($messageStack->size('password_forgotten') > 0) {
+    echo $messageStack->output('password_forgotten');
+}
+?>
+        <div id="passwordForgottenDefault-content" class="content mb-3"><?php echo TEXT_MAIN; ?></div>
 
-<?php echo zen_draw_form('password_forgotten', zen_href_link(FILENAME_PASSWORD_FORGOTTEN, 'action=process', 'SSL')); ?>
+        <div id="passwordForgottenDefault-card" class="card mb-3">
+            <h4 id="passwordForgottenDefault-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
+            <div id="passwordForgottenDefault-card-body" class="card-body p-3">
+                <div id="passwordForgottenDefault-required-info" class="required-info text-right"><?php echo FORM_REQUIRED_INFORMATION; ?></div>
 
-<?php if ($messageStack->size('password_forgotten') > 0) echo $messageStack->output('password_forgotten'); ?>
+                <label for="email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
+                <?php echo zen_draw_input_field('email_address', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="email-address" autocomplete="username" placeholder="' . ENTRY_EMAIL_ADDRESS_TEXT . '" required', 'email'); ?>
+            </div>
+        </div>
 
-<div id="passwordForgottenDefault-content" class="content"><?php echo TEXT_MAIN; ?></div>
+        <div id="passwordForgottenDefault-btn-toolbar" class="btn-toolbar justify-content-between" role="toolbar">
+            <?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>' . zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?>
+        </div>
 
-<div id="passwordForgottenDefault-card" class="card mb-3">
-<h4 id="passwordForgottenDefault-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
-<div id="passwordForgottenDefault-card-body" class="card-body p-3">
-
-<div id="passwordForgottenDefault-required-info" class="required-info text-right"><?php echo FORM_REQUIRED_INFORMATION; ?></div>
-
-<label for="email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
-<?php echo zen_draw_input_field('email_address', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="email-address" autocomplete="off" placeholder="' . ENTRY_EMAIL_ADDRESS_TEXT . '" required', 'email'); ?>
-
-</div>
-</div>
-
-<div id="passwordForgottenDefault-btn-toolbar" class="btn-toolbar justify-content-between" role="toolbar">
-<?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
-<?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?>
-</div>
-
-</form>
+    <?php echo '</form>'; ?>
 </div>
