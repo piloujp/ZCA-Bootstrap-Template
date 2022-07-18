94bcf25d9560650245bf21724f68db578ab4c16a
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-18 07:40:49 -0400

    #134: account_password, refactoring and changing autocomplete types

--- includes/templates/bootstrap/templates/tpl_account_password_default.php
+++ includes/templates/bootstrap/templates/tpl_account_password_default.php
@@ -2,7 +2,7 @@
 /**
  * Page Template
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.4.0
  *
  * Loaded automatically by index.php?main_page=account_password.<br />
  * Allows customer to change their password
@@ -15,40 +15,37 @@
  */
 ?>
 <div id="accountPasswordDefault" class="centerColumn">
+    <?php echo zen_draw_form('account_password', zen_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'), 'post', 'onsubmit="return check_form(account_password);"') . zen_draw_hidden_field('action', 'process'); ?>
 
-<?php echo zen_draw_form('account_password', zen_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'), 'post', 'onsubmit="return check_form(account_password);"') . zen_draw_hidden_field('action', 'process'); ?>
+        <div id="myPassword-card" class="card mb-3">
+            <h4 id="myPassword-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
+            <div d="myPassword-card-body" class="card-body p-3">
+                <div class="required-info text-right"><?php echo FORM_REQUIRED_INFORMATION; ?></div>
+<?php
+if ($messageStack->size('account_password') > 0) {
+    echo $messageStack->output('account_password');
+}
+?>
+                <label class="inputLabel" for="password-current"><?php echo ENTRY_PASSWORD_CURRENT; ?></label>
+                <?php echo zen_draw_password_field('password_current','','id="password-current" autocomplete="current-password" placeholder="' . ENTRY_PASSWORD_CURRENT_TEXT . '" required'); ?>
+                <div class="p-2"></div>
 
-<!--bof my password card-->
-<div id="myPassword-card" class="card mb-3">
-<h4 id="myPassword-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
-<div d="myPassword-card-body" class="card-body p-3">
+                <label class="inputLabel" for="password-new"><?php echo ENTRY_PASSWORD_NEW; ?></label>
+                <?php echo zen_draw_password_field('password_new','','id="password-new" autocomplete="new-password" placeholder="' . ENTRY_PASSWORD_NEW_TEXT . '" required'); ?>
+                <div class="p-2"></div>
 
-<div class="required-info text-right"><?php echo FORM_REQUIRED_INFORMATION; ?></div>
+                <label class="inputLabel" for="password-confirm"><?php echo ENTRY_PASSWORD_CONFIRMATION; ?></label>
+                <?php echo zen_draw_password_field('password_confirmation','','id="password-confirm" autocomplete="new-password" placeholder="' . ENTRY_PASSWORD_CONFIRMATION_TEXT . '" required'); ?>
 
-<?php if ($messageStack->size('account_password') > 0) echo $messageStack->output('account_password'); ?>
+                <div id="myPassword-btn-toolbar" class="btn-toolbar justify-content-end my-3" role="toolbar">
+                    <?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?>
+                </div>
+            </div>
+        </div>
 
-<label class="inputLabel" for="password-current"><?php echo ENTRY_PASSWORD_CURRENT; ?></label>
-<?php echo zen_draw_password_field('password_current','','id="password-current" autocomplete="off" placeholder="' . ENTRY_PASSWORD_CURRENT_TEXT . '" required'); ?>
-<div class="p-2"></div>
+        <div id="accountPasswordDefault-btn-toolbar" class="btn-toolbar my-3" role="toolbar">
+            <?php echo '<a href="' . zen_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
+        </div>
 
-<label class="inputLabel" for="password-new"><?php echo ENTRY_PASSWORD_NEW; ?></label>
-<?php echo zen_draw_password_field('password_new','','id="password-new" autocomplete="off" placeholder="' . ENTRY_PASSWORD_NEW_TEXT . '" required'); ?>
-<div class="p-2"></div>
-
-<label class="inputLabel" for="password-confirm"><?php echo ENTRY_PASSWORD_CONFIRMATION; ?></label>
-<?php echo zen_draw_password_field('password_confirmation','','id="password-confirm" autocomplete="off" placeholder="' . ENTRY_PASSWORD_CONFIRMATION_TEXT . '" required'); ?>
-
-<div id="myPassword-btn-toolbar" class="btn-toolbar justify-content-end my-3" role="toolbar">
-<?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?>
-</div>
-
-</div>
-</div>
-<!--eof my password card-->
-
-<div id="accountPasswordDefault-btn-toolbar" class="btn-toolbar my-3" role="toolbar">
-<?php echo '<a href="' . zen_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
-</div>
-
-</form>
+    <?php echo '</form>'; ?>
 </div>
