24848c890b47c86ba814371a8e4f91591c739bd5
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-18 07:14:42 -0400

    #134: Strip unwanted `<label>` tags from sidebox titles ...
    ... enabling use of the "core" sidebox processing.

--- includes/templates/bootstrap/common/tpl_box_default_left.php
+++ includes/templates/bootstrap/common/tpl_box_default_left.php
@@ -2,7 +2,7 @@
 /**
  * Common Template - tpl_box_default_left.php
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.4.0
  *
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -11,20 +11,17 @@
  * @version $Id: tpl_box_default_left.php 2975 2006-02-05 19:33:51Z birdbrain $
  */
 
-// choose box images based on box position
-  if ($title_link) {
-    $title = '<a href="' . zen_href_link($title_link) . '">' . $title . BOX_HEADING_LINKS . '</a>';
-  }
+// -----
+// The "core" Zen Cart sideboxes normally bracket the $title with a <label></label>;
+// those tags are neither needed nor wanted for the bootstrap implementation.
 //
+$title = str_replace(['<label', '</label>'], '', $title);
+
+if (!empty($title_link)) {
+    $title = '<a href="' . zen_href_link($title_link) . '">' . $title . BOX_HEADING_LINKS . '</a>';
+}
 ?>
-<!--// bof: <?php echo $box_id; ?> //-->
 <div id="<?php echo str_replace('_', '-', $box_id ) . '-leftBoxCard'; ?>" class="leftBoxCard card mb-3">
-    
-
-<h4 id="<?php echo str_replace('_', '-', $box_id) . '-leftBoxHeading'; ?>" class="leftBoxHeading card-header"><?php echo $title; ?></h4>
-
-<?php echo $content; ?>
-
+    <h4 id="<?php echo str_replace('_', '-', $box_id) . '-leftBoxHeading'; ?>" class="leftBoxHeading card-header"><?php echo $title; ?></h4>
+    <?php echo $content; ?>
 </div>
-<!--// eof: <?php echo $box_id; ?> //-->
-
