6b57954f459aaa37c3134bba6b69f4940f8792ed
lat9 <lat9@vinosdefrutastropicales.com>
2022-11-27 16:28:05 -0500

    #162: Enable (zc158a+) single-column settings to ...
    ... be displayed in the admin's "Layout Controller".

--- includes/templates/bootstrap/template_info.php
+++ includes/templates/bootstrap/template_info.php
@@ -12,5 +12,11 @@
 $template_name = 'ZCA Bootstrap-4';
 $template_version = 'Version 3.4.1';
 $template_author = 'rbarbour (zcadditions.com), lat9 (vinosdefrutastropicales.com), drbyte (zen-cart.com)';
-$template_description = 'A responsive Zen Cart template built for <em>Zen Cart <b>1.5.7</b> and later</em>.  Also validated on <em>Zen Cart <b>1.5.8</b></em>.  The template uses Bootstrap 4.6.0 and Font Awesome 5.15.1.  Additional documentation is available on the template\'s <a href="https://github.com/lat9/ZCA-Bootstrap-Template" target="_blank" rel="noopener">GitHub repository</a>.';
+$template_description = 'A responsive Zen Cart template built for <em>Zen Cart <b>1.5.7</b> and later</em>; also validated on <em>Zen Cart <b>1.5.8</b></em> and PHP 8.2.  The template uses Bootstrap 4.6.0 and Font Awesome 5.15.1.  Additional documentation is available on the template\'s <a href="https://github.com/lat9/ZCA-Bootstrap-Template" target="_blank" rel="noopener">GitHub repository</a>.';
 $template_screenshot = 'ZCA_BOOTSTRAP_TEMPLATE.png';
+
+// -----
+// This setting's effect will require a change to be provided in zc158a, essentially enabling
+// the admin's "Layout Controller" to also display the single-column layout settings.
+//
+$uses_single_column_layout_settings = true;
