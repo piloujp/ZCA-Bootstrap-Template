937b33c1f1c5719968bb6db2c29225f2caf6c0a2
John <dbltoe@users.noreply.github.com>
2023-02-14 12:35:52 -0600

    Heading Structure Fix for Acccessibility

--- includes/modules/bootstrap/centerboxes/specials_index.php
+++ includes/modules/bootstrap/centerboxes/specials_index.php
@@ -105,9 +105,9 @@ if ($num_products_count > 0) {
     $heading_month_name = sprintf(TABLE_HEADING_SPECIALS_INDEX, zca_get_translated_month_name());
     if (!empty($new_products_category_id)) {
         $category_title = zen_get_category_name((int)$new_products_category_id, $_SESSION['languages_id']);
-        $title = '<h4 id="specialCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . ($category_title != '' ? ' - ' . $category_title : '' ) . '</h4>';
+        $title = '<h3 id="specialCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . ($category_title != '' ? ' - ' . $category_title : '' ) . '</h3>';
     } else {
-        $title = '<h4 id="specialCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . '</h4>';
+        $title = '<h3 id="specialCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . '</h3>';
     }
     $zc_show_special_products = true;
 }
