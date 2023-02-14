cb946ba9b65f940fce85a4c7f9f1a9994af6a04a
John <dbltoe@users.noreply.github.com>
2023-02-14 12:34:43 -0600

    Header Structure Fix for Accessibility

--- includes/modules/bootstrap/centerboxes/new_products.php
+++ includes/modules/bootstrap/centerboxes/new_products.php
@@ -99,9 +99,9 @@ if ($num_products_count > 0) {
     $heading_month_name = sprintf(TABLE_HEADING_NEW_PRODUCTS, zca_get_translated_month_name());
     if (!empty($new_products_category_id)) {
         $category_title = zen_get_category_name((int)$new_products_category_id, $_SESSION['languages_id']);
-        $title = '<h4 id="newCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . ($category_title !== '' ? ' - ' . $category_title : '' ) . '</h4>';
+        $title = '<h3 id="newCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . ($category_title !== '' ? ' - ' . $category_title : '' ) . '</h3>';
     } else {
-        $title = '<h4 id="newCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . '</h4>';
+        $title = '<h3 id="newCenterbox-card-header" class="centerBoxHeading card-header">' . $heading_month_name . '</h3>';
     }
     $zc_show_new_products = true;
 }
