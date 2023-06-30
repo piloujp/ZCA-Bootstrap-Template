9f9ad71cef56ea40fa6a61ab94710d81043c321d
lat9 <lat9@vinosdefrutastropicales.com>
2023-06-30 18:13:55 -0400

    #254: Add spacing for price and min-max-unit elements ...
    ... on table-based product listings.

--- includes/modules/bootstrap/product_listing.php
+++ includes/modules/bootstrap/product_listing.php
@@ -333,9 +333,12 @@ if ($num_products_count > 0) {
                 case 'PRODUCT_LIST_PRICE':
                     $lc_align = ($product_listing_layout_style === 'table') ? 'right' : 'center';
 
-                    $lc_text = zen_get_products_display_price($record['products_id']);
+                    $lc_text = '<div class="pl-dp">' . zen_get_products_display_price($record['products_id']) . '</div>';
                     $lc_text .= zen_get_buy_now_button($record['products_id'], $lc_button, $more_info_button);
-                    $lc_text .= zen_get_products_quantity_min_units_display($record['products_id']);
+                    $min_max_units = zen_get_products_quantity_min_units_display($record['products_id']);
+                    if ($min_max_units !== '') {
+                        $lc_text .= '<div class="mmu-wrap">' . $min_max_units . '</div>';
+                    }
                     if (zen_get_show_product_switch($record['products_id'], 'ALWAYS_FREE_SHIPPING_IMAGE_SWITCH')) {
                         if (zen_get_product_is_always_free_shipping($record['products_id'])) {
                             $lc_text .= '<div class="text-center">';
