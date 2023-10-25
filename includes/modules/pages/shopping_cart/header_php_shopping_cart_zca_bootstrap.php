e77795fad95d62d0d77163a190f373bf2ee3dff6
lat9 <lat9@vinosdefrutastropicales.com>
2023-10-25 19:30:15 -0400

    #281: Correcting "Update Quantity" icon

--- includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
+++ includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
@@ -6,20 +6,32 @@
 // That regex "magic" says find the first '<input type="image"{...}/>', replace it with the
 // button and then copy anything else (like the 'hidden' input that follows).
 //
-// BOOTSTRAP 3.5.1.
+// BOOTSTRAP 3.6.1.
 //
 if (!(function_exists('zca_bootstrap_active') && zca_bootstrap_active())) {
     return;
 }
 
 if (!isset($productArray) || !is_array($productArray)) {
-    $productArray = []; 
+    $productArray = [];
 }
 for ($i = 0, $n = count($productArray); $i < $n; $i++) {
-    $productArray[$i]['buttonUpdate'] = preg_replace(
-        '/(<input type="image".*?\/?>)(.*)/',
-        '<button type="submit" class="btn btn-sm" aria-label="' . ICON_UPDATE_ALT . '" title="' . ICON_UPDATE_ALT . '"><i aria-hidden="true" class="fas fa-sm fa-sync-alt"></i></button>$2',
-        $productArray[$i]['buttonUpdate']
-    );
+    // -----
+    // For whatever reason, the base Zen Cart formatting for the 'buttonUpdate' entry
+    // includes a *disabled* update button when a product requires a fixed quantity
+    // to be displayed.  If that's the case, just don't include the button.
+    //
+    if ($productArray[$i]['flagShowFixedQuantity'] === true) {
+        $productArray[$i]['buttonUpdate'] = '&nbsp;';
+    } else {
+        $productArray[$i]['buttonUpdate'] = preg_replace(
+            [
+                '/<input.*type="image".*?\/?>/',
+                '/<button.*type="submit".*?>.*button>/'
+            ],
+            '<button type="submit" class="btn btn-sm" aria-label="' . ICON_UPDATE_ALT . '" title="' . ICON_UPDATE_ALT . '"><i aria-hidden="true" class="fas fa-sm fa-sync-alt"></i></button>',
+            $productArray[$i]['buttonUpdate']
+        );
+    }
     $productArray[$i]['quantityField'] = str_replace('form-control', 'form-control mx-auto text-center p-0', $productArray[$i]['quantityField']);
 }
