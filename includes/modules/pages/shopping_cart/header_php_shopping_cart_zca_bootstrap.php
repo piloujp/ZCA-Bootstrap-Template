36a5dae7dc9d02e0a6c009b6a8f2140c35f8ee95
lat9 <lat9@vinosdefrutastropicales.com>
2023-10-25 20:52:27 -0400

    #286: Correcting delete icon/checkbox overlap when fixed-quantity ...
    ... product is in the cart.

--- includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
+++ includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
@@ -23,7 +23,32 @@ for ($i = 0, $n = count($productArray); $i < $n; $i++) {
     //
     if ($productArray[$i]['flagShowFixedQuantity'] === true) {
         $productArray[$i]['buttonUpdate'] = '&nbsp;';
+
+        // -----
+        // When a product's set to show a fixed-quantity, the 'assumption' is that the incoming
+        // 'showFixedQuantityAmount' field is formatted in a manner similar to:
+        //
+        // 1<input type="hidden" name="cart_quantity[]" value="1">
+        //
+        // The fixed-quantity amount (which could also be a fractional value, e.g. 1.125) is extracted
+        // and put into a span that matches the height of any non-fixed-quantity input fields.
+        //
+        $fixed_quantity_values = preg_match('/^([0-9]+\.?[0-9]*)(.*)$/', $productArray[$i]['showFixedQuantityAmount'], $matches);
+        if ($fixed_quantity_values !== 1) {
+            trigger_error('Malformed showFixedQuantityAmount, expecting leading int/float string:' . PHP_EOL . json_encode($productArray[$i], JSON_PRETTY_PRINT), E_USER_NOTICE);
+        } else {
+            $productArray[$i]['showFixedQuantityAmount'] = '<span class="f-q-a p-2 d-block">' . $matches[1] . '</span>' . $matches[2];
+        }
     } else {
+        // -----
+        // Depending on the string-length of the ICON_UPDATE_ALT language constant, the base Zen Cart
+        // CSS button generation creates the update-icon in one of two forms, either of which is 'converted'
+        // into an FA icon:
+        //
+        // <button type="submit" class="btn button_update_cart">Update</button>
+        //
+        // <input type="image" src="includes/templates/template_default/buttons/english/button_update_cart.png" alt="Change your quantity by highlighting the number in the box, correcting the quantity and clicking this button." title="Change your quantity by highlighting the number in the box, correcting the quantity and clicking this button.">
+        //
         $productArray[$i]['buttonUpdate'] = preg_replace(
             [
                 '/<input.*type="image".*?\/?>/',
