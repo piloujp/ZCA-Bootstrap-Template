dbcd7468aad93491b404cd613b89f48b9acb9991
lat9 <lat9@vinosdefrutastropicales.com>
2024-02-01 10:54:18 -0500

    #313: Include alpha-filter on products_all, if enabled

--- includes/templates/bootstrap/templates/tpl_modules_listing_display_order.php
+++ includes/templates/bootstrap/templates/tpl_modules_listing_display_order.php
@@ -31,11 +31,11 @@ if ($disp_order !== $disp_order_default) {
     array_unshift($display_order_options, ['id' => $disp_order_default, 'text' => PULL_DOWN_ALL_RESET]);
 }
 ?>
-<div id="listingDisplayOrderSorter" class="mb-3">
-    <label for="disp-order-sorter"><?php echo TEXT_INFO_SORT_BY; ?></label>
+<div id="listingDisplayOrderSorter" class="row mb-3">
+    <label for="disp-order-sorter" class="mx-2"><?php echo TEXT_INFO_SORT_BY; ?></label>
 <?php
 echo
-    zen_draw_form('sorter_form', zen_href_link($_GET['main_page']), 'get') .
+    zen_draw_form('sorter_form', zen_href_link($_GET['main_page']), 'get', 'class="form-inline"') .
     zen_draw_hidden_field('main_page', $_GET['main_page']) .
     zen_hide_session_id();
 if (isset($_GET['cPath'], $cPath)) {
--- includes/templates/bootstrap/templates/tpl_products_all_default.php
+++ includes/templates/bootstrap/templates/tpl_products_all_default.php
@@ -2,7 +2,7 @@
 /**
  * Page Template
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP 3.6.3
  *
  * @package templateSystem
  * @copyright Copyright 2003-2016 Zen Cart Development Team
@@ -12,12 +12,34 @@
  */
 ?>
 <div id="productsAllDefault" class="centerColumn">
-
-<h1 id="productsAllDefault-pageHeading" class="pageHeading"><?php echo HEADING_TITLE; ?></h1>
-
+    <h1 id="productsAllDefault-pageHeading" class="pageHeading"><?php echo HEADING_TITLE; ?></h1>
+    <div class="row">
 <?php
-require($template->get_template_dir('/tpl_modules_listing_display_order.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_listing_display_order.php'); ?>
+if (PRODUCT_LIST_ALPHA_SORTER === 'true') {
+?>
+        <div class="col">
+<?php
+    echo zen_draw_form('filter', zen_href_link(FILENAME_SEARCH_RESULT), 'get', 'class="form-inline"');
+    echo '<label class="inputLabel mx-2">' . TEXT_SHOW . '</label>';
 
-<?php require($template->get_template_dir('tpl_modules_product_listing.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_product_listing.php'); ?>
+    /* Redisplay all $_GET variables, except currency */
+    echo zen_post_all_get_params('currency');
 
+    require DIR_WS_MODULES . zen_get_module_directory(FILENAME_PRODUCT_LISTING_ALPHA_SORTER);
+
+    echo '</form>';
+?>
+        </div>
+<?php
+}
+?>
+        <div class="col">
+<?php
+require $template->get_template_dir('/tpl_modules_listing_display_order.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_modules_listing_display_order.php';
+?>
+        </div>
+    </div>
+<?php
+require $template->get_template_dir('tpl_modules_product_listing.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_modules_product_listing.php';
+?>
 </div>
