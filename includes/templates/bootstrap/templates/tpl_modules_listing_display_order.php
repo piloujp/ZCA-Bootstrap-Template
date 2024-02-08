39c77b73366073171fd933662b19412819f9243b
lat9 <lat9@vinosdefrutastropicales.com>
2024-02-08 17:00:53 -0500

    #307: Align layout with zc200 solution

--- includes/templates/bootstrap/templates/tpl_modules_listing_display_order.php
+++ includes/templates/bootstrap/templates/tpl_modules_listing_display_order.php
@@ -11,13 +11,20 @@
  * @version $Id: tpl_modules_listing_display_order.php 3369 2006-04-03 23:09:13Z drbyte $
  */
 $disp_order = (int)($disp_order ?? 0);
-$disp_order_default = (int)($disp_order_default ?? 0);
 if ($disp_order <= 0 || $disp_order > 8) {
-    $disp_order = ($disp_order_default > 0 && $disp_order_default < 8) ? $disp_order_default : 1;
+    $disp_order = 8;
 }
 
+// -----
+// Language constant, added in v200, define here if not previously defined; can be
+// removed once support for ZC versions < 2.0.0 is dropped.
+//
+if (!defined('TEXT_INFO_SORT_BY_RECOMMENDED')) {
+    define('TEXT_INFO_SORT_BY_RECOMMENDED', 'Recommended');
+}
 // NOTE: to remove a sort order option add a PHP comment around the option to be removed
 $display_order_options = [
+    ['id' => '8', 'text' => TEXT_INFO_SORT_BY_RECOMMENDED],
     ['id' => '1', 'text' => TEXT_INFO_SORT_BY_PRODUCTS_NAME],
     ['id' => '2', 'text' => TEXT_INFO_SORT_BY_PRODUCTS_NAME_DESC],
     ['id' => '3', 'text' => TEXT_INFO_SORT_BY_PRODUCTS_PRICE],
@@ -26,10 +33,6 @@ $display_order_options = [
     ['id' => '6', 'text' => TEXT_INFO_SORT_BY_PRODUCTS_DATE_DESC],
     ['id' => '7', 'text' => TEXT_INFO_SORT_BY_PRODUCTS_DATE],
 ];
-
-if ($disp_order !== $disp_order_default) {
-    array_unshift($display_order_options, ['id' => $disp_order_default, 'text' => PULL_DOWN_ALL_RESET]);
-}
 ?>
 <div id="listingDisplayOrderSorter" class="row mb-3">
     <label for="disp-order-sorter" class="mx-2"><?php echo TEXT_INFO_SORT_BY; ?></label>
