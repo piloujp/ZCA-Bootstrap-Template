e7210fa6bbb739a3177725f21c974b50536d3513
lat9 <lat9@vinosdefrutastropicales.com>
2024-02-08 14:01:58 -0500

    #305: Enabling a fluid display on category listings

--- includes/modules/bootstrap/category_row.php
+++ includes/modules/bootstrap/category_row.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BOOTSTRAP v3.4.0
+ * BOOTSTRAP v3.6.3
  *
  * index category_row.php
  *
@@ -10,45 +10,95 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2021 Jul 28 Modified in v1.5.8-alpha $
+ * @version $Id: lat9 2022 Jul 18 Modified in v1.5.8-alpha2 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
-$title = '';
+
 $num_categories = $categories->RecordCount();
 
-$row = 0;
-$col = 0;
+$rows = 0;
+$columns = 0;
 $list_box_contents = [];
-if ($num_categories > 0) {
-    if ($num_categories < MAX_DISPLAY_CATEGORIES_PER_ROW || (int)MAX_DISPLAY_CATEGORIES_PER_ROW === 0) {
-        $col_width = floor(100/$num_categories);
-    } else {
-        $col_width = floor(100/MAX_DISPLAY_CATEGORIES_PER_ROW);
+$title = '';
+
+if (empty($num_categories)) {
+    return;
+}
+
+$columns_per_row = defined('MAX_DISPLAY_CATEGORIES_PER_ROW') ? (int)MAX_DISPLAY_CATEGORIES_PER_ROW : 0;
+$category_row_layout_style = $columns_per_row > 1 ? 'columns' : 'fluid';
+
+// if in fixed-columns mode, calculate column width
+if ($category_row_layout_style === 'columns') {
+    $calc_value = $columns_per_row;
+    if ($num_categories < $columns_per_row || $columns_per_row == 0) {
+        $calc_value = $num_categories;
+    }
+    $col_width = floor(100 / $calc_value) - 0.5;
+}
+
+foreach ($categories as $next_category) {
+    $zco_notifier->notify('NOTIFY_CATEGORY_ROW_IMAGE', $next_category['categories_id'], $next_category['categories_image']);
+    if (empty($next_category['categories_image'])) {
+        $next_category['categories_image'] = 'pixel_trans.gif';
+    }
+    $cPath_new = zen_get_path($next_category['categories_id']);
+
+    // strip out 0_ from top level cats
+    $cPath_new = str_replace('=' . (int)TOPMOST_CATEGORY_PARENT_ID . '_', '=', $cPath_new);
+
+    // -----
+    // Starting with v3.6.3, a categories' fluid layout can be identified.  If predefined (like
+    // in an /extra_datafiles .php module), that override is used.
+    //
+    if ($category_row_layout_style === 'fluid') {
+        $grid_cards_classes = 'row-cols-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3';
+        if (!isset($grid_category_classes_matrix)) {
+            // this array is intentionally in reverse order, with largest index first
+            $grid_category_classes_matrix = [
+                '12' => 'row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6',
+                '10' => 'row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-5',
+                '9' => 'row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-5',
+                '8' => 'row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4',
+                '6' => 'row-cols-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3',
+            ];
+        }
+
+        // determine classes to use based on number of grid-columns used by "center" column
+        if (isset($center_column_width)) {
+            foreach ($grid_category_classes_matrix as $width => $classes) {
+                if ($center_column_width >= $width) {
+                    $grid_cards_classes = $classes;
+                    break;
+                }
+            }
+        }
+        $list_box_contents[$rows]['params'] = 'class="row ' . $grid_cards_classes . ' text-center"';
     }
 
-    foreach ($categories as $next_category) {
-        $zco_notifier->notify('NOTIFY_CATEGORY_ROW_IMAGE', $next_category['categories_id'], $next_category['categories_image']); 
-        if (empty($next_category['categories_image'])) {
-            $next_category['categories_image'] = 'pixel_trans.gif';
-        }
-        $cPath_new = zen_get_path($next_category['categories_id']);
+    $style = '';
+    if ($category_row_layout_style === 'columns') {
+        $style = ' style="width:' . $col_width . '%;"';
+    }
+    $list_box_contents[$rows][] = [
+        'params' => 'class="categoryListBoxContents card mb-3 p-3 text-center"' . $style,
+        'text' =>
+            '<a href="' . zen_href_link(FILENAME_DEFAULT, $cPath_new) . '">' .
+                zen_image(DIR_WS_IMAGES . $next_category['categories_image'], $next_category['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT, 'loading="lazy"') .
+                '<br>' .
+                $next_category['categories_name'] .
+            '</a>',
+        'wrap_with_classes' => 'col',
+        'card_type' => $category_row_layout_style,
+    ];
 
-        // strip out 0_ from top level cats
-        $cPath_new = str_replace('=0_', '=', $cPath_new);
-
-        //    $categories->fields['products_name'] = zen_get_products_name($categories->fields['products_id']);
-
-        $list_box_contents[$row][$col] = [
-            'params' => 'class="categoryListBoxContents card mb-3 p-3 text-center"',
-            'text' => '<a href="' . zen_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . zen_image(DIR_WS_IMAGES . $next_category['categories_image'], $next_category['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT, 'loading="lazy"') . '<br>' . $next_category['categories_name'] . '</a>'
-        ];
-
-        $col++;
-        if ($col >= MAX_DISPLAY_CATEGORIES_PER_ROW) {
-            $col = 0;
-            $row++;
+    if ($category_row_layout_style === 'columns') {
+        $columns++;
+        if ($columns >= $columns_per_row) {
+            $columns = 0;
+            $rows++;
         }
     }
 }
