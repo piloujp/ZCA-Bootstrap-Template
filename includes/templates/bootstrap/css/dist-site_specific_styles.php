5f8cbab8ef5b53599273aa297e7729df55a4a515
lat9 <lat9@vinosdefrutastropicales.com>
2024-02-10 22:53:58 -0500

    #315: Correct "dead" link in site-specific-styles.css

--- includes/templates/bootstrap/css/dist-site_specific_styles.php
+++ includes/templates/bootstrap/css/dist-site_specific_styles.php
@@ -1,13 +1,13 @@
 <?php 
 /*
- * BOOTSTRAP 3.3.0
+ * BOOTSTRAP 3.6.4
  *
  * Create a file called "site_specific_styles.php" to contain any changes
  * to base css provided by this template. Place site-specific content
  * between the opening and closing style tags.
  *
- * Refer to https://github.com/lat9/ZCA-Bootstrap-Template/blob/v300/pages/faqs.md for
- * additional information.
+ * Refer to https://github.com/lat9/ZCA-Bootstrap-Template/wiki/Frequently-Asked-Questions#applying-site-specific-styling
+ * for additional information.
  */
 ?>
 <style>
