40099b8c7bff32136a42d83a95deb78cc38a8db3
lat9 <lat9@vinosdefrutastropicales.com>
2024-03-06 10:31:37 -0500

    #326: Add wrapper for also-purchased centerbox

--- includes/templates/bootstrap/centerboxes/tpl_modules_also_purchased_products.php
+++ includes/templates/bootstrap/centerboxes/tpl_modules_also_purchased_products.php
@@ -2,7 +2,7 @@
 /**
  * Module Template
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.6.4
  *
  * Displays content related to "also-purchased-products"
  *
@@ -12,12 +12,38 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_modules_also_purchased_products.php 3206 2006-03-19 04:04:09Z birdbrain $
  */
-  $zc_show_also_purchased = false;
-  include(DIR_WS_MODULES . zen_get_module_directory('centerboxes/' . FILENAME_ALSO_PURCHASED_PRODUCTS));
-?>
+$zc_show_also_purchased = false;
+require DIR_WS_MODULES . zen_get_module_directory('centerboxes/' . FILENAME_ALSO_PURCHASED_PRODUCTS);
 
-<?php if ($zc_show_also_purchased == true) { ?>
-<?php
-  require($template->get_template_dir('tpl_columnar_display.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/tpl_columnar_display.php');
+if ($zc_show_also_purchased === false) {
+    return;
+}
 ?>
-<?php } ?>
+<div id="also-purchased" class="card mb-3 centerBoxWrapper">
+<?php
+    if (!empty($title)) {
+        echo $title;
+    }
+?>
+    <div id="also-purchased-card-body" class="card-body text-center">
+<?php
+    if (is_array($list_box_contents)) {
+        for ($row = 0, $n = count($list_box_contents); $row < $n; $row++) {
+            $params = '';
+?>
+        <div class="card-deck text-center">
+<?php
+            for ($col = 0, $j = count($list_box_contents[$row]); $col < $j; $col++) {
+                $r_params = $list_box_contents[$row][$col]['params'];
+                if (isset($list_box_contents[$row][$col]['text'])) {
+                    echo '<div ' . $r_params . '>' . $list_box_contents[$row][$col]['text'] . '</div>';
+                }
+            }
+?>
+        </div>
+<?php
+        }
+    }
+?>
+    </div>
+</div>
