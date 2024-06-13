da51dc9935bdbd3ba5addbcf238f45822ee3238d
lat9 <lat9@vinosdefrutastropicales.com>
2024-06-12 21:56:04 -0400

    #392: Correct `gv_faq` page's form-entry default.
    ... also changing to use short-echo tags.

--- includes/templates/bootstrap/templates/tpl_gv_faq_default.php
+++ includes/templates/bootstrap/templates/tpl_gv_faq_default.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BOOTSTRAP v3.7.0
+ * BOOTSTRAP v3.7.1
  *
  * Displays the FAQ pages for the Gift-Certificate/Voucher system.<br />
  *
@@ -18,28 +18,28 @@ if (!empty($customer_has_gv_balance)) {
 }
 ?>
     <div id="giftCertificateFaq-card" class="card mb-3">
-        <h4 id="giftCertificateFaq-card-header" class="card-header"><?php echo HEADING_TITLE; ?></h4>
+        <h4 id="giftCertificateFaq-card-header" class="card-header"><?= HEADING_TITLE ?></h4>
         <div id="giftCertificateFaq-card-body" class="card-body p-3"> 
-            <div id="giftCertificateFaq-content" class="content"><?php echo TEXT_INFORMATION; ?></div>
-            <h2 id="giftCertificateFaq-subHeading" class="pageSubHeading"><?php echo $subHeadingTitle; ?></h2>
-            <div id="giftCertificateFaq-content-two" class="content"><?php echo $subHeadingText; ?></div>
+            <div id="giftCertificateFaq-content" class="content"><?= TEXT_INFORMATION ?></div>
+            <h2 id="giftCertificateFaq-subHeading" class="pageSubHeading"><?= $subHeadingTitle ?></h2>
+            <div id="giftCertificateFaq-content-two" class="content"><?= $subHeadingText ?></div>
             <div id="giftCertificateFaq-btn-toolbar" class="btn-toolbar my-3" role="toolbar">
-                <?php echo zca_back_link(); ?>
+                <?= zca_back_link() ?>
             </div>
         </div>
     </div>
 
     <div id="giftCertificateRedemption-card" class="card">
-        <h4 id="giftCertificateRedemption-card-header" class="card-header"><?php echo TEXT_GV_REDEEM_INFO; ?></h4>
+        <h4 id="giftCertificateRedemption-card-header" class="card-header"><?= TEXT_GV_REDEEM_INFO ?></h4>
         <div id="giftCertificateRedemption-card-body" class="card-body">
-            <form action="<?php echo zen_href_link(FILENAME_GV_REDEEM, '', 'NONSSL', false); ?>" method="get">
-                <?php echo zen_draw_hidden_field('main_page',FILENAME_GV_REDEEM) . zen_draw_hidden_field('goback','true') . zen_hide_session_id(); ?>
+            <form action="<?= zen_href_link(FILENAME_GV_REDEEM, '', 'NONSSL', false) ?>" method="get">
+                <?= zen_draw_hidden_field('main_page', FILENAME_GV_REDEEM) . zen_draw_hidden_field('goback', 'true') . zen_hide_session_id() ?>
 
-                <label class="inputLabel" for="lookup-gv-redeem"><?php echo TEXT_GV_REDEEM_ID; ?></label>
-                <?php echo zen_draw_input_field('gv_no', isset($_GET['gv_no']) ? $_GET['gv_no'] : '0', 'size="18" id="lookup-gv-redeem"');?>
+                <label class="inputLabel" for="lookup-gv-redeem"><?= TEXT_GV_REDEEM_ID ?></label>
+                <?= zen_draw_input_field('gv_no', '', 'size="18" id="lookup-gv-redeem"') ?>
 
                 <div id="giftCertificateRedemption-btn-toolbar" class="btn-toolbar justify-content-end my-3" role="toolbar">
-                    <?php echo zen_image_submit(BUTTON_IMAGE_REDEEM, BUTTON_REDEEM_ALT); ?>
+                    <?= zen_image_submit(BUTTON_IMAGE_REDEEM, BUTTON_REDEEM_ALT) ?>
                 </div>
             </form>
         </div>
