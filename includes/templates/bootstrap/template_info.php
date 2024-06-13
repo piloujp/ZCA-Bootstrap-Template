775e941016a9dc6adf36944a695656639b2ddeae
lat9 <lat9@vinosdefrutastropicales.com>
2024-06-12 20:43:48 -0400

    #389: Removing date-related comments from template_info.php

--- includes/templates/bootstrap/template_info.php
+++ includes/templates/bootstrap/template_info.php
@@ -2,17 +2,17 @@
 /**
  * Template Information File
  * 
- * BOOTSTRAP v3.7.0
+ * BOOTSTRAP v3.7.1 (last version *this file* was updated); See ZCA_BOOTSTRAP_CURRENT_VERSION in
+ *  /admin/includes/init_includes/init_zca_bootstrap_template_admin.php for the "real" current version.
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
  */
 $template_name = 'ZCA Bootstrap-4';
-$template_version = 'Version ' . (defined('ZCA_BOOTSTRAP_CURRENT_VERSION') ? ZCA_BOOTSTRAP_CURRENT_VERSION : '3.6.2');
+$template_version = 'Version ' . (defined('ZCA_BOOTSTRAP_CURRENT_VERSION') ? ZCA_BOOTSTRAP_CURRENT_VERSION : '3.7.1');
 $template_author = 'rbarbour (zcadditions.com), lat9 (vinosdefrutastropicales.com), drbyte (zen-cart.com)';
-$template_description = 'A responsive Zen Cart template built for <em>Zen Cart <b>1.5.8, 1.5.8a and v2.0.0</b></em> and PHP versions 7.4 through 8.3.  The template uses Bootstrap 4.6.2 and Font Awesome 6.5.2.  Additional documentation is available on the template\'s <a href="https://github.com/lat9/ZCA-Bootstrap-Template" target="_blank" rel="noreferrer noopener">GitHub repository</a>.';
+$template_description = 'A responsive Zen Cart template built for <em>Zen Cart <b>1.5.8, 1.5.8a and v2.0.x</b></em> and PHP versions 7.4 through 8.3.  The template uses Bootstrap 4.6.2 and Font Awesome 6.5.2.  Additional documentation is available on the template\'s <a href="https://github.com/lat9/ZCA-Bootstrap-Template" target="_blank" rel="noreferrer noopener">GitHub repository</a>.';
 $template_screenshot = 'ZCA_BOOTSTRAP_TEMPLATE.png';
 
 // -----
