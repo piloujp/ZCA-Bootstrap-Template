23be8a101ddee7394c8b0f30181b03530f222249
lat9 <lat9@vinosdefrutastropicales.com>
2024-06-16 13:11:13 -0400

    #399: Use short-echo tags

--- includes/templates/bootstrap/templates/tpl_account_default.php
+++ includes/templates/bootstrap/templates/tpl_account_default.php
@@ -15,24 +15,26 @@
  */
 ?>
 <div id="accountDefault" class="centerColumn">
-    <h1 id="accountDefault-pageHeading" class="pageHeading"><?php echo HEADING_TITLE; ?></h1>
-
-<?php if ($messageStack->size('account') > 0) echo $messageStack->output('account'); ?>
-
+    <h1 id="accountDefault-pageHeading" class="pageHeading"><?= HEADING_TITLE ?></h1>
+<?php
+if ($messageStack->size('account') > 0) {
+    echo $messageStack->output('account');
+}
+?>
     <div class="card-deck mb-3">
 <!--bof my account card links-->
         <div id="myAccount-card" class="card">
-            <h4 id="myAccount-card-header" class="card-header"><?php echo MY_ACCOUNT_TITLE; ?></h4>
+            <h4 id="myAccount-card-header" class="card-header"><?= MY_ACCOUNT_TITLE ?></h4>
             <div id="myAccount-card-body" class="card-body p-3">
                 <ul id="myAccount-list-group" class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <?php echo zca_button_link(zen_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'), MY_ACCOUNT_INFORMATION); ?>
+                        <?= zca_button_link(zen_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'), MY_ACCOUNT_INFORMATION) ?>
                     </li>
                     <li class="list-group-item">
-                       <?php echo zca_button_link(zen_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'), MY_ACCOUNT_ADDRESS_BOOK); ?>
+                       <?= zca_button_link(zen_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'), MY_ACCOUNT_ADDRESS_BOOK) ?>
                     </li>
                     <li class="list-group-item">
-                        <?php echo zca_button_link(zen_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'), MY_ACCOUNT_PASSWORD); ?>
+                        <?= zca_button_link(zen_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'), MY_ACCOUNT_PASSWORD) ?>
                     </li>
                 </ul>
             </div>
@@ -43,14 +45,14 @@ if ((int)ACCOUNT_NEWSLETTER_STATUS > 0 || CUSTOMERS_PRODUCTS_NOTIFICATION_STATUS
 ?>
 <!--bof email notifications card links-->
         <div id="emailNotifications-card" class="card">
-            <h4 id="emailNotifications-card-header" class="card-header"><?php echo EMAIL_NOTIFICATIONS_TITLE; ?></h4>
+            <h4 id="emailNotifications-card-header" class="card-header"><?= EMAIL_NOTIFICATIONS_TITLE ?></h4>
             <div id="emailNotifications-card-body" class="card-body p-3">
                 <ul id="emailNotifications-list-group" class="list-group list-group-flush">
 <?php
     if ((int)ACCOUNT_NEWSLETTER_STATUS > 0) {
 ?>
                     <li class="list-group-item">
-                        <?php echo zca_button_link(zen_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'), EMAIL_NOTIFICATIONS_NEWSLETTERS); ?>
+                        <?= zca_button_link(zen_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'), EMAIL_NOTIFICATIONS_NEWSLETTERS) ?>
                     </li>
 <?php 
     } //endif newsletter unsubscribe
@@ -58,7 +60,7 @@ if ((int)ACCOUNT_NEWSLETTER_STATUS > 0 || CUSTOMERS_PRODUCTS_NOTIFICATION_STATUS
     if (CUSTOMERS_PRODUCTS_NOTIFICATION_STATUS === '1') {
 ?>
                     <li class="list-group-item">
-                        <?php echo zca_button_link(zen_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL'), EMAIL_NOTIFICATIONS_PRODUCTS); ?>
+                        <?= zca_button_link(zen_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL'), EMAIL_NOTIFICATIONS_PRODUCTS) ?>
                     </li>
 <?php 
     } //endif product notification
@@ -81,10 +83,10 @@ if (count($ordersArray) !== 0) {
 ?>
 <!--bof previous orders card -->
     <div id="previousOrders-card" class="card mb-3">
-        <h4 id="previousOrders-card-header" class="card-header"><?php echo OVERVIEW_PREVIOUS_ORDERS; ?></h4>
+        <h4 id="previousOrders-card-header" class="card-header"><?= OVERVIEW_PREVIOUS_ORDERS ?></h4>
         <div id="previousOrders-card-body" class="card-body p-3">
             <div id="previousOrders-helpLink" class="helpLink text-right p-3">
-                <a href="<?php echo zen_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'); ?>"><?php echo OVERVIEW_SHOW_ALL_ORDERS; ?></a>
+                <a href="<?= zen_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') ?>"><?= OVERVIEW_SHOW_ALL_ORDERS ?></a>
             </div>
             <div class="card-deck">
 <?php
@@ -93,18 +95,18 @@ if (count($ordersArray) !== 0) {
 ?>
                 <div class="card">
                     <div class="card-header text-center">
-                        <a class="orderIdCell" href="<?php echo $order_link; ?>"><?php echo TEXT_NUMBER_SYMBOL . $orders['orders_id']; ?></a>
+                        <a class="orderIdCell" href="<?= $order_link ?>"><?= TEXT_NUMBER_SYMBOL . $orders['orders_id'] ?></a>
                     </div>
                     <div class="card-body text-center">
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item dateCell border-0 p-1"><?php echo zen_date_short($orders['date_purchased']); ?></li>
-                            <li class="list-group-item shipToCell border-0 p-1"><?php echo zen_output_string_protected($orders['order_name']) . '<br>' . $orders['order_country']; ?></li>
-                            <li class="list-group-item statusCell border-0 p-1"><?php echo $orders['orders_status_name']; ?></li>
-                            <li class="list-group-item border-0 p-1"><?php echo $orders['order_total']; ?></li>
+                            <li class="list-group-item dateCell border-0 p-1"><?= zen_date_short($orders['date_purchased']) ?></li>
+                            <li class="list-group-item shipToCell border-0 p-1"><?= zen_output_string_protected($orders['order_name']) . '<br>' . $orders['order_country'] ?></li>
+                            <li class="list-group-item statusCell border-0 p-1"><?= $orders['orders_status_name'] ?></li>
+                            <li class="list-group-item border-0 p-1"><?= $orders['order_total'] ?></li>
                         </ul>
                     </div>
                     <div class="card-footer text-center">
-                        <?php echo zca_button_link($order_link, BUTTON_VIEW_SMALL_ALT, 'button_view'); ?>
+                        <?= zca_button_link($order_link, BUTTON_VIEW_SMALL_ALT, 'button_view') ?>
                     </div>
                 </div>
 <?php
