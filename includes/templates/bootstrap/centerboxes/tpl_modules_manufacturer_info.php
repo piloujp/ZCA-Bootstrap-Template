46fb180ae2b234afe9e3fed1db65e663310ad198
lat9 <lat9@vinosdefrutastropicales.com>
2024-08-29 14:33:05 -0400

    #432: Correcting notifications/manufacturers-info layout inconsistencies (#433)
    * Correct inconsistencies
    
    * ... and reformatting/refactoring
    
    - Use short-echo tags
    - Use short-array syntax
    - Split long-ish lines

--- includes/templates/bootstrap/centerboxes/tpl_modules_manufacturer_info.php
+++ includes/templates/bootstrap/centerboxes/tpl_modules_manufacturer_info.php
@@ -2,7 +2,7 @@
 /**
  * Side Box Template
  * 
- * BOOTSTRAP v3.4.0
+ * BOOTSTRAP v3.7.3
  *
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -14,24 +14,28 @@ $mi_id = $manufacturer_info_sidebox->fields['manufacturers_id'];
 $mi_name = $manufacturer_info_sidebox->fields['manufacturers_name'];
 ?>
 <div id="manufacturerInfo-centerBoxContents" class="card mb-3 text-center">
-    <h4 id="manufacturerInfo-centerBoxHeading" class="centerBoxHeading card-header"><?php echo BOX_HEADING_MANUFACTURER_INFO; ?></h4>
+    <div id="manufacturerInfo-centerBoxHeading" class="centerBoxHeading card-header h4">
+        <?= BOX_HEADING_MANUFACTURER_INFO ?>
+    </div>
     <div id="manufacturerInfo-card-body" class="card-body p-3 text-center">
-        <div class="text-center mb-3"><?php echo zen_image(DIR_WS_IMAGES . $manufacturer_info_sidebox->fields['manufacturers_image'], $mi_name); ?></div>
+        <div class="text-center mb-3">
+            <?= zen_image(DIR_WS_IMAGES . $manufacturer_info_sidebox->fields['manufacturers_image'], $mi_name) ?>
+        </div>
         <ul class="list-group">
 <?php
 if (!empty($manufacturer_info_sidebox->fields['manufacturers_url'])) {
 ?>
             <li class="list-group-item">
-                <a href="<?php echo zen_href_link(FILENAME_REDIRECT, 'action=manufacturer&manufacturers_id=' . $mi_id); ?>" target="_blank">
-                    <?php echo sprintf(BOX_MANUFACTURER_INFO_HOMEPAGE, $mi_name); ?>
+                <a href="<?= zen_href_link(FILENAME_REDIRECT, 'action=manufacturer&manufacturers_id=' . $mi_id) ?>" target="_blank">
+                    <?= sprintf(BOX_MANUFACTURER_INFO_HOMEPAGE, $mi_name) ?>
                 </a>
             </li>
 <?php
 }
 ?>
             <li class="list-group-item">
-                <a href="<?php echo zen_href_link(FILENAME_DEFAULT, 'manufacturers_id=' . $mi_id); ?>">
-                    <?php echo BOX_MANUFACTURER_INFO_OTHER_PRODUCTS; ?>
+                <a href="<?= zen_href_link(FILENAME_DEFAULT, 'manufacturers_id=' . $mi_id) ?>">
+                    <?= BOX_MANUFACTURER_INFO_OTHER_PRODUCTS ?>
                 </a>
             </li>
         </ul>
--- includes/templates/bootstrap/centerboxes/tpl_modules_no_notifications.php
+++ includes/templates/bootstrap/centerboxes/tpl_modules_no_notifications.php
@@ -2,7 +2,7 @@
 /**
  * Side Box Template
  * 
- * BOOTSTRAP v3.5.2
+ * BOOTSTRAP v3.7.3
  *
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -12,12 +12,14 @@
  */
 ?>
 <div id="ProductNotifications-centerBoxContents" class="card mb-3 text-center">
-    <h3 id="ProductNotifications-centerBoxHeading" class="centerBoxHeading card-header"><?php echo BOX_HEADING_NOTIFICATIONS; ?></h3>
+    <div id="ProductNotifications-centerBoxHeading" class="centerBoxHeading card-header h4">
+        <?= BOX_HEADING_NOTIFICATIONS ?>
+    </div>
     <div id="ProductNotifications-card-body" class="card-body p-3 text-center">
-        <a href="<?php echo zen_href_link($_GET['main_page'], zen_get_all_get_params(array('action')) . 'action=notify', $request_type); ?>" title="<?php echo OTHER_BOX_NOTIFY_YES_ALT; ?>">
-            <?php echo zen_image(DIR_WS_TEMPLATE_IMAGES . OTHER_IMAGE_BOX_NOTIFY_YES, OTHER_BOX_NOTIFY_YES_ALT); ?>
+        <a href="<?= zen_href_link($_GET['main_page'], zen_get_all_get_params(['action']) . 'action=notify', $request_type) ?>" title="<?= OTHER_BOX_NOTIFY_YES_ALT ?>">
+            <?= zen_image(DIR_WS_TEMPLATE_IMAGES . OTHER_IMAGE_BOX_NOTIFY_YES, OTHER_BOX_NOTIFY_YES_ALT) ?>
             <br>
-            <?php echo sprintf(BOX_NOTIFICATIONS_NOTIFY, zen_get_products_name($_GET['products_id'])); ?>
+            <?= sprintf(BOX_NOTIFICATIONS_NOTIFY, zen_get_products_name($_GET['products_id'])) ?>
         </a>
     </div>
 </div>
