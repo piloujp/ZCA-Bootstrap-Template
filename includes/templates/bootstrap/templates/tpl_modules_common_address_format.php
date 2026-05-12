4883eee66e545d0a38b125881db442c1bbb7377d
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-13 00:23:30 +0900

    Update to ZCA Bootstrap Template v3.7.10
    State field not required update

--- includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
+++ includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
@@ -103,7 +103,7 @@ if (ACCOUNT_COMPANY === 'true') {
 if (ACCOUNT_STATE === 'true') {
     if ($flag_show_pulldown_states === true) {
 ?>
-<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><?php if (!empty(ENTRY_STATE_TEXT)) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>' ?>
+<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><span class="alert"><?= ((!empty(ENTRY_STATE_TEXT) && (int)ENTRY_STATE_MIN_LENGTH > 0) ? ENTRY_STATE_TEXT : '') ?></span>
 
 <?php
       echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"');
@@ -115,7 +115,7 @@ if (ACCOUNT_STATE === 'true') {
 
 <label class="inputLabel" for="state" id="stateLabel"><?= $state_field_label ?></label>
 <?php
-    echo zen_draw_input_field('state', '', zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_state', '40') . ' id="state" class="form-control" placeholder="' . ENTRY_STATE_TEXT . '"');
+    echo zen_draw_input_field('state', '', zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_state', '40') . ' id="state" class="form-control"' . ((int)ENTRY_STATE_MIN_LENGTH > 0 ? ' placeholder="' . ENTRY_STATE_TEXT . '"' : ''));
     if ($flag_show_pulldown_states === false) {
         echo zen_draw_hidden_field('zone_id', $zone_name, ' ');
     }
